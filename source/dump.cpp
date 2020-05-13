83ea78df3613e44f85259adf23123ef43572579c
Ethan Worth <eworth23@my.whitworth.edu>
2020-05-12 18:09:11 -0700

    ready to compile?

--- source/dump.cpp
+++ source/dump.cpp
@@ -14,6 +14,8 @@
 
 using std::vector, std::queue, std::set, std::pair, std::string, std::cout;
 
+bool DEBUG(true);
+
 // valid colors: red, orange, yellow, green, blue, purple
 struct Color {
     char c;
@@ -42,16 +44,30 @@ struct Action {
 };
 
 struct Airport {
-    string name;
+    string name;        // used to check start and end, key and display name
     float lat, lon;
     // routes stored as <Airport*, Color>
     vector<pair<Airport*, Color>> children, parents;
     vector<Airplane*> occupants;
     vector<Airplane*> shadow_occupants;
-};
 
-struct Option {
-    int score, actions_used;
+    Airport(string n, float lati = 0, float longi = 0) : 
+        name(n),
+        lat(lati),
+        lon(longi),
+        occupants(0, NULL),
+        shadow_occupants(0, NULL) {}
+
+    static void connect(Airport* parent, Airport* child, Color c) {
+        // enforce right to left motion
+        if(child->lon > parent->lon) {
+            std::swap(parent, child);
+        }
+
+        // add connections
+        parent->children.push_back(std::make_pair(child, c));
+        child->parents.push_back(std::make_pair(parent, c));
+    }
 };
 
 // Airplane Class
@@ -90,11 +106,15 @@ class Airplane {
 
         // updates loc, returns nothing for now
         bool move(Airport* dest, bool main_board) {
-            
             // remove this plane from the old location
+            if(main_board && DEBUG) {
+                cout << "Moved from " << location->name << " to " << dest->name << ". ";
+            }
             std::remove(get_occupancy_special(get_location_special(main_board), main_board).begin(), get_occupancy_special(get_location_special(main_board), main_board).end(), this);
             // add it to the new one
             get_occupancy_special(dest, main_board).push_back(this);
+            // update location
+            get_location_special(main_board) = dest;
             return true;            
         }
 
@@ -164,9 +184,15 @@ class Airplane {
         bool do_action(Action act, bool main_board) {
             // move takeoff
             if(act.type == 'T') {
+                if(main_board && DEBUG) {
+                    cout << "Take-off to " << act.info << "! ";
+                }
                 return jump(act.info, main_board);
             }
             else if(act.type == 'C') {
+                if(main_board && DEBUG) {
+                    cout << act.info << " from " << location->name << ". ";
+                }
                 return move_route(*(new Color(act.info)), main_board);
             }
             else {
@@ -184,12 +210,15 @@ class Airplane {
             if(occ.size() > 1) {
                 // for each plane
                 for(int i = 0; i < occ.size(); i++) {
-                    // if it is a different plane
-                    if(occ[i] != this) {
+                    // if it is a different plane and we aren't at the start or end
+                    if( occ[i] != this && !(get_location_special(main_board)->name == "START" || get_location_special(main_board)->name == "END") ) {
                         // with a different owner
                         if(occ[i]->owner != owner) {
                             occ[i]->bumped(main_board);
                             did_the_thing = true;
+                            if(main_board && DEBUG) {
+                                cout << "Bumped on " << location->name << "! ";
+                            }
                         }
                         // if same owner, problem!
                         else {
@@ -235,6 +264,7 @@ class Player {
         vector<Airplane> planes;
         Color plane_color;
         const float BUMP_VALUE = 20;
+        bool finished;
 
         // returns vector with Actions to be taken this turn
         vector<Action> roll(int dice = 2) {
@@ -268,6 +298,14 @@ class Player {
                 }
             }
 
+            if(DEBUG) {
+                cout << "[ "; 
+                for(int i = 0; i < actions.size(); i++) {
+                    cout << "(" << actions[i].type << ", " << actions[i].info << "), ";
+                }
+                cout << " ]"; 
+            }
+
             // return vector of actions
             return actions;
         }
@@ -276,9 +314,9 @@ class Player {
         float get_plane_score(Airplane a, bool main_board) {
             // really good if at end
             if(a.get_loc_name(main_board) == "END") {
-                return 150;
+                return 250;
             }
-            return a.get_loc(main_board)->lat;
+            return -(a.get_loc(main_board)->lon);
         }
 
         // given a set of actions, returns best movement plan
@@ -405,7 +443,8 @@ class Player {
         Player(Game* g, vector<Airplane> p, Color c)  :
             game(g), 
             planes(p), 
-            plane_color(c) {
+            plane_color(c),
+            finished(false) {
                 for(int i = 0; i < planes.size(); i++) {
                     planes[i].set_owner_ptr(this);
                 }
@@ -452,9 +491,24 @@ class Player {
                 }
             }
 
-            // check for bumps
+            // check for bumps and planes at end
             for(int i = 0; i < planes.size(); i++) {
+                // delete planes at end
+                if(planes[i].get_loc_name(true) == "END") {
+                    planes.erase(planes.begin()+i);
+                    if(planes.size() == 0) {
+                        finished == true;
+                        if(DEBUG) {
+                            cout << "A Player has finished!\n";
+                        }
+                    }
+                }
+                // check for bumps
                 planes[i].check_bump(true);
+            }
+
+            if(DEBUG) {
+                cout << "\n\n";
             }
         }
 
@@ -468,6 +522,10 @@ class Player {
         Game* get_game() const {
             return game;
         }
+
+        bool is_done() const {
+            return finished;
+        }
 };
 
 class Game {
@@ -476,14 +534,15 @@ class Game {
         std::unordered_map<string, Airport> map;
         queue<string> takeoff_pile;
         vector<Player> players;
-        // vector<Airplane> planes;
         const int NUM_OF_PLAYERS, PLANES_PER_PLAYER, MAX_TURNS;
         int turn_location, turn_num;
     //public:
-        Game(int player_count = 4, int planes_per_player = 4, int max_turns = -1) : 
+        Game(int player_count = 4, int planes_per_player = 4, int max_turns = 100) : 
             NUM_OF_PLAYERS(player_count), 
             PLANES_PER_PLAYER(planes_per_player), 
-            MAX_TURNS(max_turns) {
+            MAX_TURNS(max_turns),
+            turn_location(0),
+            turn_num(0) {
                 srand(time(0));
                 // initialize players
                 for(int i = 0; i < player_count; i++) {
@@ -491,14 +550,68 @@ class Game {
                     vector<Airplane> temp_planes(planes_per_player);
                     // initialize player and add to vector
                     players.emplace_back(this, temp_planes, (new Color())->DEFUALT_COLORS[i]);  // can't really make a static const array, so I have to do this to access default colors
-                    /*for(int i = 0; i < temp_planes.size(); i++) {
-                        planes.push_back(temp_planes[i]);
-                    }*/
                 }
                 // generate Airports and connections
                     // end goal: pull airport data from .txt file and generate connections automatically
                     // intermediate: pull airport data from .txt file, data includes connections
                     // basic: manually initialize airports and connections
+                // make airports
+                    map.insert(std::make_pair("START", Airport("START", 21.318611, 202.0775)));
+                    map.insert(std::make_pair("Atlanta", Airport("Atlanta", 33.636667, -84.428056)));
+                    map.insert(std::make_pair("Los Angeles", Airport("Los Angeles", 33.9425, -118.408056)));
+                    map.insert(std::make_pair("New York", Airport("New York", 40.639722, -73.778889)));
+                    map.insert(std::make_pair("Beijing", Airport("Beijing", 40.0725, 116.5975)));
+                    map.insert(std::make_pair("Dubai", Airport("Dubai", 25.252778, 55.364444)));
+                    map.insert(std::make_pair("London", Airport("London", 51.4775, -0.461389)));
+                    map.insert(std::make_pair("Sydney", Airport("Sydney", -33.946111, 151.177222)));
+                    map.insert(std::make_pair("Kemton Park", Airport("Kemton Park", -26.133333, 28.25)));
+                    map.insert(std::make_pair("END", Airport("END", 21.318611, -157.9225)));
+
+                // make connections
+                    Airport::connect(&(map.at("START")), &(map.at("Beijing")), Color("red"));
+                    Airport::connect(&(map.at("START")), &(map.at("Sydney")), Color("green"));
+                    Airport::connect(&(map.at("Beijing")), &(map.at("Sydney")), Color("orange"));
+                    Airport::connect(&(map.at("Dubai")), &(map.at("Sydney")), Color("yellow"));
+                    Airport::connect(&(map.at("Dubai")), &(map.at("Beijing")), Color("yellow"));
+                    Airport::connect(&(map.at("Kemton Park")), &(map.at("Beijing")), Color("purple"));
+                    Airport::connect(&(map.at("Kemton Park")), &(map.at("Sydney")), Color("yellow"));
+                    Airport::connect(&(map.at("London")), &(map.at("Beijing")), Color("red"));
+                    Airport::connect(&(map.at("London")), &(map.at("Dubia")), Color("orange"));
+                    Airport::connect(&(map.at("London")), &(map.at("Kemton Park")), Color("green"));
+                    Airport::connect(&(map.at("Dubai")), &(map.at("Kemton Park")), Color("purple"));
+                    Airport::connect(&(map.at("London")), &(map.at("New York")), Color("blue"));
+                    Airport::connect(&(map.at("London")), &(map.at("Altanta")), Color("green"));
+                    Airport::connect(&(map.at("Dubai")), &(map.at("Atlanta")), Color("red"));
+                    Airport::connect(&(map.at("Dubai")), &(map.at("New York")), Color("yellow"));
+                    Airport::connect(&(map.at("Kemton Park")), &(map.at("Atlanta")), Color("red"));
+                    Airport::connect(&(map.at("Los Angeles")), &(map.at("Atlanta")), Color("orange"));
+                    Airport::connect(&(map.at("New York")), &(map.at("Atlanta")), Color("blue"));
+                    Airport::connect(&(map.at("END")), &(map.at("Atlanta")), Color("purple"));
+                    Airport::connect(&(map.at("New York")), &(map.at("Los Angeles")), Color("yellow"));
+                    Airport::connect(&(map.at("New York")), &(map.at("END")), Color("red"));
+                    Airport::connect(&(map.at("END")), &(map.at("Los Angeles")), Color("green"));
+
+
+                // initialize take-off pile
+                for(auto it = map.begin(); it != map.end(); it++) {
+                    takeoff_pile.push((*it).first);
+                }
+        }
+
+        // game loop
+        void loop() {
+            bool someone_left(true);
+            // while ther are turn and players left
+            for(turn_num = 0; turn_num != MAX_TURNS && someone_left; turn_num++) {
+                someone_left = false;
+                // have each player take their turn
+                for(turn_location = 0; turn_location < players.size(); turn_location++) {
+                    if(!players[turn_location].is_done()) {
+                        players[turn_location].take_turn();
+                        someone_left = true;
+                    }
+                }
+            }
         }
 
         // simulate drawing a take-off card
@@ -523,5 +636,6 @@ class Game {
 };
 
 int main() {
-    
+    Game test(1, 1);
+    test.loop();
 }
